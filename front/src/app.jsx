13b5c6c50d79ac2c0b64fb89dc922cfc1089773a
nabeliwo <hiroki.c.watanabe@gmail.com>
2016-10-21 01:20:00 +0900

    :bug: fix bug that the order of reset.css and base.css is not guaranteed in the css to be output

--- front/src/app.jsx
+++ front/src/app.jsx
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import App from './components/frame/App/App';
 
 import setting from './styles/setting.css'; // eslint-disable-line no-unused-vars
-import reset from './styles/reset.css'; // eslint-disable-line no-unused-vars
 import base from './styles/base.css'; // eslint-disable-line no-unused-vars
 
 render(<App />, document.getElementById('app'));
