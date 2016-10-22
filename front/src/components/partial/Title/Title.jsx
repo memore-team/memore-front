28e89edf42d36e5da6d2a19fcfea1f8bd0748510
nabeliwo <hiroki.c.watanabe@gmail.com>
2016-10-22 20:51:09 +0900

    :sparkles: Implements Title Component of variable size

--- front/src/components/partial/Title/Title.jsx
+++ front/src/components/partial/Title/Title.jsx
@@ -1,10 +1,39 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 import styles from './Title.css';
 
-const Title = ({ value, theme }) => (
-  <input className={`${styles.title} ${theme.title}`} defaultValue={value} />
-);
+const LINE_HEIGHT = 30;
+let textAreaEl = null;
+
+function changeHeight(e) {
+  textAreaEl.style.height = `${LINE_HEIGHT}px`;
+
+  const lineNum = e.target.scrollHeight / LINE_HEIGHT;
+  textAreaEl.style.height = `${lineNum * LINE_HEIGHT}px`;
+}
+
+class Title extends Component {
+  componentDidMount() {
+    textAreaEl = document.getElementById('js-textarea');
+    changeHeight({ target: textAreaEl });
+  }
+
+  render() {
+    const { theme, value } = this.props;
+
+    return (
+      <div className={`${styles.title} ${theme.title}`}>
+        <textarea
+          id="js-textarea"
+          style={{ lineHeight: `${LINE_HEIGHT}px` }}
+          className={styles.text}
+          defaultValue={value}
+          onChange={changeHeight}
+        />
+      </div>
+    );
+  }
+}
 
 Title.propTypes = {
   value: PropTypes.string.isRequired,
